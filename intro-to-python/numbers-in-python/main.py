1f1429be3ba08693d7ce8f5ecf9e953cc2335168
kienonline19 <kienonline19.haui@gmail.com>
2022-07-01 20:34:44 +0700

    change

---
+++ intro-to-python/numbers-in-python/main.py
@@ -0,0 +1,14 @@
+age = 35 # 35 is integer
+print(age)
+
+PI = 3.14159 # 3.14159 is float
+print(PI)
+
+maths_operation = 1 + 3 * 4 / 2 - 2
+print(maths_operation)
+
+float_division = 8 / 3
+print(float_division)
+
+integer_division = 8 // 3
+print(integer_division)
