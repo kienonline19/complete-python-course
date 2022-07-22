199a102d3a36ef9909ffe0f1e066483095b521fe
kienonline19 <kienonline19.haui@gmail.com>
2022-07-22 13:34:10 +0700

    commit

--- problems/trang-chu-problem/main.py
+++ problems/trang-chu-problem/main.py
@@ -1,9 +1,13 @@
 import re
 import datetime
 
-MENU_PROMPT = """Please enter 1 for Sign up.
+MAIN_MENU_PROMPT = """Please enter 1 for Sign up.
 Please enter 2 for Log in.
 Please enter 3 for Exit.
+"""
+
+SUB_MENU_PROMPT = """Please enter 1 for Resetting the Password.
+Please enter 2 for Signout.
 """
 
 
@@ -20,7 +24,7 @@ users = []
 past = set()
 
 while True:
-    user_option = int(input(MENU_PROMPT))
+    user_option = int(input(MAIN_MENU_PROMPT))
 
     if user_option == 1:
         full_name = input("Please enter your name:")
@@ -46,13 +50,12 @@ while True:
             "Please Enter your Date of Birth # DD/MM/YYYY (No Space):")
 
         while True:
-
             flag_mobile_number = len(mobile_number) != 10 or \
-                not mobile_number.isdigit() or \
-                not mobile_number.startswith('0')
+                                 not mobile_number.isdigit() or \
+                                 not mobile_number.startswith('0')
 
             flag_password = not re.match(r'[A-Za-z]+[@$#]+\d+', password)
-            flag_password_confirm = not(password == password_confirm)
+            flag_password_confirm = not (password == password_confirm)
             flag_age = True
 
             try:
@@ -90,8 +93,8 @@ while True:
                 print()
 
             if all(
-                flag == False
-                for flag in (flag_mobile_number, flag_password, flag_password_confirm, flag_date_of_birth, flag_age)
+                    flag == False
+                    for flag in (flag_mobile_number, flag_password, flag_password_confirm, flag_date_of_birth, flag_age)
             ):
                 if email_id not in past:
                     users.append(SignUp(
@@ -100,14 +103,86 @@ while True:
                         mobile_number,
                         password,
                         date_of_birth
-                    ))
+                    ).__dict__)
 
                 past.add(email_id)
                 print("You have Successfully Signed up.")
                 break
 
     elif user_option == 2:
-        pass
+        user_name = input("Please enter your Username (Mobile Number):")
+        print()
+        user_password = input("Please enter your password:")
+
+        if len(users) != 0:
+            for user in users:
+                if user['mobile_number'] == user_name and \
+                        user['password'] == user_password:
+                    print("You have successfully Signed in")
+                    print(f"Welcome {user['full_name']}")
+
+                    sub_user_option = int(input(SUB_MENU_PROMPT))
+                    if sub_user_option == 1:
+                        flag = False
+
+                        for _ in range(3):
+                            sub_user_name = input(
+                                "Please enter your Username (Mobile Number):")
+                            print()
+                            old_password = input(
+                                "Please enter your old password:")
+                            print()
+                            new_password = input(
+                                "Please enter your new password:")
+                            check = bool(re.match(r'[A-Za-z]+[@$#]+\d+', new_password))
+
+                            if sub_user_name == user_name and \
+                                    check and \
+                                    old_password == user_password:
+                                user['password'] = new_password
+                                print("Your Password has been reset successfully.")
+                                flag = True
+
+                        if not flag:
+                            print("You have used the maximum attempts of Login:")
+                            print("Please reset the password by entering the below details\n")
+
+                            username_mn = input("Please enter your Username (Mobile Number) to confirm:")
+                            flag1 = user_name != username_mn
+                            if flag1:
+                                print("You have entered the wrong Username.")
+
+                            print()
+                            dob = input("Please enter your Date of Birth in DD/MM/YYYY format, to confirm:")
+                            flag2 = dob != user['date_of_birth']
+                            if flag2:
+                                print("You have entered the wrong Date of Birth.")
+
+                            print()
+                            n_pass = input("Please enter your new password:")
+                            flag3 = not re.match(r'[A-Za-z]+[@$#]+\d+', n_pass)
+                            if flag3:
+                                print("You have entered the new password in Invalid format.")
+
+                            print()
+                            re_pass = input("Please re-enter your new password:")
+                            flag4 = re_pass != n_pass
+                            if flag4:
+                                print("Your passwords are not matching.")
+
+                            if n_pass == user_password:
+                                print("You cannot use the password used earlier.")
+                                break
+                            elif all(flag == False for flag in (flag1, flag2, flag3, flag4)):
+                                print("Your Password has been reset successfully.")
+                                user['password'] = n_pass
+                            else:
+                                break
+
+                    elif sub_user_option == 2:
+                        print("Thank You for using the Application.")
+                        break
+
     elif user_option == 3:
         print("Thank You for using the Application.")
         break
