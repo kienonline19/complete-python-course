d4e3be904af481dcacb9858cbc31aea6e715424c
kienonline19 <kienonline19.haui@gmail.com>
2022-10-09 07:44:22 +0700

    commit

--- practice/giap-phong-01/utils/reading_data_file.py
+++
@@ -1,8 +0,0 @@
-def get_input_data():
-    with open('data/inp.txt', 'r', encoding='utf-8') as fr:
-        return [line.strip() for line in fr]
-
-
-def read_common_pattern():
-    with open('data/COMMON_PATTERN.DAT', 'r', encoding='utf-8') as fcp:
-        return fcp.read()
--- practice/mr-dat-problems-01/cau3.py
+++
@@ -1,5 +0,0 @@
-kt_slp = lambda x: set(str(x)) == set('68')
-
-for n in range(10000):
-    if kt_slp(n):
-        print(n, end=' ')
